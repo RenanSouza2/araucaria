6e4db11577e347b848914c1dd7d2d4354acd1b78
renansouza-cheesecake <renan.souza@cheesecake.us>
2026-08-05 10:07:10 -0300

    makefile

--- makefiles/vars.mk
+++ makefiles/vars.mk
@@ -1,8 +1,18 @@
 PRJ_NAME = araucaria
-PRJ_DIR ?= $(shell git rev-parse --show-toplevel)
+
+# Immediate (:=) so git runs once per make rather than on every expansion, and
+# unconditional (not ?=) so an inherited PRJ_DIR from a parent project's
+# sub-make cannot win and silently point at the wrong project.
+PRJ_DIR := $(shell git rev-parse --show-toplevel)
+
+DIR := $(patsubst $(PRJ_DIR)/%,%,$(CURDIR))
+
 LIB_DIR = $(PRJ_DIR)/lib
-DBG_FULL_FILE = $(LIB_DIR)/debug_full.o
-DBG_FILE = $(LIB_DIR)/debug.o
+CLU_DIR = $(PRJ_DIR)/mods/clu/bin
+
 LIB_FILE = $(LIB_DIR)/lib.o
-CLU_FILE = $(PRJ_DIR)/mods/clu/bin/clu.o
-DIR = $(patsubst %/,%,$(shell git rev-parse --show-prefix))
+DBG_FILE = $(LIB_DIR)/debug.o
+
+DBG_FULL_FILE = $(LIB_DIR)/debug_full.o
+
+CLU_FILE = $(CLU_DIR)/clu.o
